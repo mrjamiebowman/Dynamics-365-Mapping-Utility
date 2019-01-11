d8e72db0122ec934184f444efb8550c63aa4fb49
Jamie Bowman <mrjamiebowman@gmail.com>
2019-01-11 17:08:55 -0500

    Account Controller changes.

--- D365.Samples.WebHooks/Controllers/AccountController.cs
+++ D365.Samples.WebHooks/Controllers/AccountController.cs
@@ -1,17 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 
 namespace D365.Samples.WebHook.Controllers {
     [Route("api/[controller]")]
     [ApiController]
     public class AccountController : ControllerBase {
+        [HttpGet]
+        public string Get() {
+            return "Dynamics 365 Entity/WebHook AutoMapper";
+        }
+
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value) {
-
+        public void Post([FromBody] JObject data) {
+            string test = "";
+            //JObject postImage = data?.PostEntityImages[0]?.value;
         }
     }
 }
