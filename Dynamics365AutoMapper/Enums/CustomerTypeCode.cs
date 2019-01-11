93dae7913a3992cc189dbf2193300ac301f87f86
Jamie Bowman <mrjamiebowman@gmail.com>
2019-01-10 20:22:51 -0500

    Created enums folder with CustomTypeCode type.

---
+++ Dynamics365AutoMapper/Enums/CustomerTypeCode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dynamics365AutoMapper.Enums {
+    public enum CustomerTypeCode {
+        Competitor = 1,
+        Consultant = 2,
+        Customer = 3,
+        Investor = 4,
+        Partner = 5,
+        Influencer = 6,
+        Press = 7,
+        Prospect = 8,
+        Reseller = 9,
+        Supplier = 10,
+        Vendor = 11,
+        Other = 12
+    }
+}
