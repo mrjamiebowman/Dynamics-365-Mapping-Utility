d1438f7ca670ab29c0f46e47e76be8a40da4e9cf
Jamie Bowman <mrjamiebowman@gmail.com>
2019-01-12 00:25:50 -0500

    Created CrmEntityAttribute for class metadata.

---
+++ Dynamics365AutoMapper/Attributes/CrmEntityAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dynamics365AutoMapper.Attributes {
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public class CRMEntityAttribute : System.Attribute {
+        public string EntityLogicalName;
+
+        public CRMEntityAttribute() {
+
+        }
+
+        public CRMEntityAttribute(string entityLogicalName) {
+            this.EntityLogicalName = entityLogicalName;
+        }
+    }
+}
