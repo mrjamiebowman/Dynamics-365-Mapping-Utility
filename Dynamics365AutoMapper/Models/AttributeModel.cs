121fabd17594faff40ae17df6c95b5ca52fdf9ce
Jamie Bowman <mrjamiebowman@gmail.com>
2019-01-12 00:02:22 -0500

    Created CRM Attribute Model

---
+++ Dynamics365AutoMapper/Models/AttributeModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dynamics365AutoMapper.Models {
+    public class AttributeModel {
+        public string key { get; set; }
+        public object value { get; set; }
+    }
+}
